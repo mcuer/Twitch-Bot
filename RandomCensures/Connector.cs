a6c4b6c674a81aebddc76b11f9e639217b6ee9c3
Thomas <thomas.roy33@ynov.com>
2017-02-09 17:39:49 +0100

    Connector + Stream

--- RandomCensures/Connector.cs
+++ RandomCensures/Connector.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RandomCensures
 {
-    public class Class1
+    class Connector
     {
     }
 }
---
+++ RandomCensures/Stream.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace RandomCensures
+{
+    public class Stream
+    {
+        public StreamReader reader
+        {
+            get
+            {
+                return this.reader;
+            }
+            private set
+            {
+                this.reader = value;
+            }
+        }
+            
+        public StreamWriter writer
+        {
+            get
+            {
+                return this.writer;
+            }
+            private set
+            {
+                this.writer = value;
+            }
+        }
+        
+        public Stream (Stream str)
+        {
+            this.reader = str.reader;
+            this.writer = str.writer;
+        }
+
+        public Stream (StreamReader reader, StreamWriter writer)
+        {
+            this.reader = reader;
+            this.writer = writer;
+        }
+    }
+}
