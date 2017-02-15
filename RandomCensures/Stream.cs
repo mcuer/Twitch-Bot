187f0314e16a04780fb877b979c02ae76d30d0b6
Thomas <thomas.roy33@ynov.com>
2017-02-15 09:29:08 +0100

    Stream constructor + Reconnect

--- RandomCensures/Stream.cs
+++ RandomCensures/Stream.cs
@@ -31,12 +31,91 @@ namespace RandomCensures
                 this.writer = value;
             }
         }
-        
-        public Stream ()
+
+        public string oAuth
+        {
+            get
+            {
+                return this.oAuth;
+            }
+            private set
+            {
+                this.oAuth = value;
+            }
+        }
+
+        public string chatMessagePrefix
+        {
+            get
+            {
+                return this.chatMessagePrefix;
+            }
+            private set
+            {
+                this.chatMessagePrefix = value;
+            }
+        }
+
+        public string chatCommandId
+        {
+            get
+            {
+                return this.chatCommandId;
+            }
+            private set
+            {
+                this.chatCommandId = value;
+            }
+        }
+
+        public string userName
+        {
+            get
+            {
+                return this.userName;
+            }
+            private set
+            {
+                this.userName = value;
+            }
+        }
+
+        public string channelName
+        {
+            get
+            {
+                return this.channelName;
+            }
+            private set
+            {
+                this.channelName = value;
+            }
+        }
+
+        public Stream (string uName, string oAuth )
+        {
+            this.userName = uName.ToLower();
+            this.channelName = this.userName;
+            this.oAuth = oAuth;
+            this.chatCommandId = "PRIVMSG";
+            this.chatMessagePrefix = $":{userName}!{userName}@{userName}.tmi.twitch.tv {chatCommandId} #{channelName}";
+
+        }
+
+        private void Reconnect()
         {
             TcpClient tcpClient = new TcpClient("irc.twitch.tv", 6667);
             this.reader = new StreamReader(tcpClient.GetStream());
             this.writer = new StreamWriter(tcpClient.GetStream());
+
+            writer.AutoFlush = true;
+            writer.WriteLine(
+                    "PASS " + oAuth + Environment.NewLine
+                    + "NICK " + userName + Environment.NewLine
+                    + "USER " + userName + " 8 * :" + userName
+                );
+            writer.WriteLine("CAP REQ :twitch.tv/membership");
+            writer.WriteLine("JOIN #" + channelName);
         }
     }
 }
